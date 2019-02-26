0fea60def1fce098f4ad6dd7e4a3d366eb2b2bca
alex-skinner <alex.skins@gmail.com>
2019-02-26 11:45:12 +0000

    added console log to fib.js

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,6 +35,7 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
+    console.log(this.state.seenIndexes);
     if (this.state.seenIndexes && this.state.seenIndexes.length > 0) {
       return this.state.seenIndexes.map(({ number }) => number).join(', ');
     }
