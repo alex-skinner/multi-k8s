2834cbc6e55be3ca7181c3df1c8acb6dc975a808
alex-skinner <alex.skins@gmail.com>
2019-02-26 11:05:29 +0000

    added a conditional check to seen indexes to stop site from crashing

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,10 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    if (this.state.seenIndexes) {
+      return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    }
+    return;
   }
 
   renderValues() {
